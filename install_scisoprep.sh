dc992e65cbe3da95ff0d223ea3f4affba64507b0
ArthurDondi <arthur.dondi@gmail.com>
2023-02-16 14:28:41 +0100

    python 3.10

--- install_scisoprep.sh
+++ install_scisoprep.sh
@@ -11,7 +11,8 @@ wget https://github.com/ConesaLab/SQANTI3/archive/refs/tags/v5.1.1.tar.gz
 tar -xvf v5.1.1.tar.gz
 
 cd SQANTI3-5.1.1/
-sed -i 's/numpy/numpy=1.19.5/' SQANTI3.conda_env.yml 
+sed -i 's/numpy/numpy=1.19.5/' SQANTI3.conda_env.yml
+sed -i 's/>=3.7.6/=3.10.7/' SQANTI3.conda_env.yml 
 conda env create -f SQANTI3.conda_env.yml -n scIsoPrep
 
 
